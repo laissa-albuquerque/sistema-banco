2c9ed831eeab55841bffd654bc34fddbc467c4b7
laissa-albuquerque <laissaalbuquerque@gmail.com>
2021-06-17 10:53:28 -0300

    alteração no titulo da pagina inicial do swagger

--- src/main/java/com/accenture/academico/sistemabanco/config/SwaggerConfig.java
+++ src/main/java/com/accenture/academico/sistemabanco/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.accenture.academico.sistemabanco.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,8 +17,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any()).build();
 	}
+	
+	private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("")
+	            .version("1.0.0")
+	            .contact(new Contact("", "", ""))
+                .title("Agile Bank")
+                .description("Api que fornece operações bancárias.")
+                .build();
+    }
 
 }
